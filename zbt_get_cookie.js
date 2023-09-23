9ad6b7ec83891adb0573949e2c5c40371df8f5ca
quiet <630876811@qq.com>
2023-09-24 00:33:44 +0800

    update

--- zbt_get_cookie.js
+++ zbt_get_cookie.js
@@ -12,7 +12,7 @@ When TieBa app is opened, click "My", If notification gets cookie success, you c
 Note that the following config is only a local script configuration, please put this scripts into Quantumult X/Script, and the cookie script only works for TieBa apps in china apple store
 
 [rewrite_local]
-https?:\/\/kunpeng.business.tech.sznyjd.cn url script-request-header https://github.com/miaojunmiao/quanx/blob/master/zbt_get_cookie.js
+https?:\/\/kunpeng.business.tech.sznyjd.cn url script-request-header https://raw.githubusercontent.com/miaojunmiao/quanx/master/zbt_get_cookie.js
 
 # MITM = kunpeng.business.tech.sznyjd.cn
 
